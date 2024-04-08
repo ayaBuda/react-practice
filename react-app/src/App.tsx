d292c16f9683265acca0c587f4b4fb92a53bc3bb
Ayanda Buda <aybuda022@student.wethinkcode.co.za>
2024-04-08 16:36:57 +0200

    added the basic files for the project and some basic code lines

---
+++ react-app/src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message"
+
+function App(){
+  return <div><Message></Message></div>
+}
+
+//This allows it to be used somewhere else
+export default App;
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing
+function Message(){
+    //JSX: JavaScript XML
+    const name = "Ayanda";
+    if (name)
+        return <h1>Hey {name}!</h1>;
+    return <h1>Hello World!</h1>
+}
+
+export default Message; 
